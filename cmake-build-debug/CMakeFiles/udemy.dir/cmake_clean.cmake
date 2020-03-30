10cf89984c2c7f3b5b1c2d276d1917acd1a3b41e
Oyiat <joris.lefondeur@hotmail.com>
2020-03-30 18:51:51 +0200

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/udemy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/udemy.dir/main.c.o"
+  "udemy.pdb"
+  "udemy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/udemy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
