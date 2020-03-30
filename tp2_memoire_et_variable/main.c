c4caca6b193c7a658dd1ad3b1de64132d91b5721
Oyiat <joris.lefondeur@hotmail.com>
2020-03-30 23:29:08 +0200

    Add main.c from tp2 to executable, done the tp2 exercise and delete main.c from the others tps.

--- tp2_memoire_et_variable/main.c
+++ tp2_memoire_et_variable/main.c
@@ -1,3 +1,26 @@
-int main(){
-return 0;
+#include <stdio.h>
+
+int main()
+{
+    printf("TP2 : Memoire et variable\n");
+
+    int mon_int = 8;
+    long mon_long = 32;
+    short mon_short = 16;
+
+    float mon_float = 3.14;
+    double mon_double = 88.8;
+
+    char mon_char = 'J';
+
+    printf("Mon int = %d et fait %lu octets\n", mon_int, sizeof(int));
+    printf("Mon long = %ld et fait %lu octets\n", mon_long, sizeof(long));
+    printf("Mon short = %d et fait %lu octets\n", mon_short, sizeof(short));
+
+    printf("Mon float = %f et fait %lu octets\n",mon_float, sizeof(float));
+    printf("Mon double = %f et fait %lu octets\n", mon_double, sizeof(double));
+
+    printf("Mon char = %c et fait %lu octets\n",mon_char, sizeof(char));
+
+    return 0;
 }
