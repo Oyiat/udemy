c2301e3da41fce0437b0ae0089e75504da4a21b9
Oyiat <joris.lefondeur@hotmail.com>
2020-03-31 16:21:31 +0200

    Add tp5 and exercise pair_ou_impair both completed

---
+++ exercice/pair_ou_impair/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Exercice: Pair ou impair\n");
+
+    int entier;
+    printf("Donnez un nombre entier: ");
+    scanf("%d", &entier);
+
+    if(entier%2 != 0){
+        printf("%d est un nombre impair", entier);
+    } else {
+        printf("%d est un nombre pair", entier);
+    }
+
+    return 0;
+}
+
+
--- tp5_conditions/main.c
+++ tp5_conditions/main.c
@@ -2,5 +2,37 @@
 
 int main(){
     printf("TP5 : Conditions\n");
+
+    int nombre1, nombre2;
+    char operateur;
+
+    printf("Indiquez l'operation mathematique que je doit resoudre (sans espace)\n\t-sous la forme [nombre1 opérateur nombre2] par exemple (2+3)\n\t-operateurs possibles [+, -, *, /, %%]\n");
+    scanf("%d%c%d", &nombre1, &operateur, &nombre2);
+
+    switch (operateur){
+        case '+':
+            printf("Trop simple! Le resultat est: %d",nombre1+nombre2);
+            break;
+        case '-':
+            printf("Trop simple! Le resultat est: %d",nombre1-nombre2);
+            break;
+        case '*':
+            printf("Trop simple! Le resultat est: %d",nombre1*nombre2);
+            break;
+        case '/':
+            if(nombre2 != 0) {
+                printf("Trop simple! Le resultat est: %f", (float) nombre1 / (float) nombre2);
+            } else {
+                printf("Attention la division par 0 est impossible!");
+            }
+            break;
+        case '%':
+            printf("Trop simple! Le resultat est: %d",nombre1%nombre2);
+            break;
+        default:
+            printf("Opérateur invalide, vous devez utilise l'un des opérateurs (+, -, *, /, %%)");
+    }
+
+
     return 0;
 }
